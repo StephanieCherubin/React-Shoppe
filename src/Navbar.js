9b2990f5d9becf7b2cd4d2ca81d0b3be5196695c
Stephanie Cherubin <seraphin.cherubin@gmail.com>
2019-05-08 16:23:28 -0700

    add links to navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,11 +2,30 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
+import brand from './A_Mr_Magoriums_Wonder_Emporium_logo.png';
 
 const Navbar = () => (
   <div className="navbar">
-    <Link to="/" className="logo">Wonder Emporium</Link>
-    <Link to="/shop" className="logo">Shop</Link>
+    <Link to="/" className="brand">
+      <img
+        alt="brandName"
+        className="brand"
+        src={brand}
+      />
+    </Link>
+    <Link to="/shop" className="list">
+      <i className="fas fa-shopping-cart" />
+        Cart
+    </Link>
+    <Link to="/shop" className="list">Shop</Link>
+    <Link to="/shop" className="list">
+      <i className="fas fa-bookmark" />
+        Wish List
+    </Link>
+    <Link to="/shop" className="list">
+      <i className="fas fa-user-alt" />
+        Account
+    </Link>
   </div>
 );
 
