b7ee6b31e13e82062be4d6645eedff8681d54b62
Stephanie Cherubin <seraphin.cherubin@gmail.com>
2019-05-08 17:24:44 -0700

    add Footer component

---
+++ src/Footer.js
@@ -0,0 +1,32 @@
+/* eslint-disable react/jsx-filename-extension */
+/* eslint-disable react/jsx-no-undef */
+import React from 'react';
+import { Link } from 'react-router-dom';
+import brand from './A_Mr_Magoriums_Wonder_Emporium_logo.png';
+
+const Footer = () => (
+  <div className="navbar">
+    <Link to="/" className="brand">
+      <img
+        alt="brandName"
+        className="brand"
+        src={brand}
+      />
+    </Link>
+    <Link to="/shop" className="list">
+      <i className="fas fa-shopping-cart" />
+        Cart
+    </Link>
+    <Link to="/shop" className="list">Shop</Link>
+    <Link to="/shop" className="list">
+      <i className="fas fa-bookmark" />
+        Wish List
+    </Link>
+    <Link to="/shop" className="list">
+      <i className="fas fa-user-alt" />
+        Account
+    </Link>
+  </div>
+)
+
+export default Footer;
