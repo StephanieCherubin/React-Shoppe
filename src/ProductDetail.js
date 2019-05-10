39d41552f7216705ef06e9c5796c4ca4b4a8988c
Stephanie Cherubin <seraphin.cherubin@gmail.com>
2019-05-09 21:22:08 -0700

    add new page

---
+++ src/ProductDetail.js
@@ -0,0 +1,38 @@
+/* eslint-disable react/jsx-filename-extension */
+import React from 'react';
+import PropTypes from 'prop-types';
+import inventory from './Inventory';
+
+const ProductDetail = ({ match }) => {
+  const { id } = match.params;
+  // const id = match.params.id;
+
+  function exact(product) {
+    return product.id === Number(id);
+  }
+
+  const index = inventory.findIndex(exact);
+
+  const item = inventory[index];
+  return (
+    <div className="productDetail">
+      <img
+        alt="product_image"
+        className="productImg"
+        src={`https://picsum.photos/200?random=${id}`}
+      />
+      <h3>{item.name}</h3>
+      <p className="description">{item.description}</p>
+      <p className="price">
+        $
+        {item.price}
+      </p>
+    </div>
+  );
+};
+
+ProductDetail.propTypes = {
+  match: PropTypes.number.isRequired,
+};
+
+export default ProductDetail;
