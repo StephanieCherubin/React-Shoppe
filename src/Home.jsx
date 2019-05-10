9db267d282b09333e1ac082ab24fc007d567f57f
Stephanie Cherubin <seraphin.cherubin@gmail.com>
2019-05-09 21:22:27 -0700

    edit js to jsx

---
+++ src/Home.jsx
@@ -0,0 +1,20 @@
+/* eslint-disable react/jsx-filename-extension */
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Home = ({ id }) => {
+  const styles = {
+    backgroundImage: `url(https://picsum.photos/1200/580?random=${id}`,
+    backgroundSize: 'cover',
+    height: '100vh',
+  };
+  return (
+    <div style={styles} />
+  );
+};
+
+Home.propTypes = {
+  id: PropTypes.number.isRequired,
+};
+
+export default Home;
