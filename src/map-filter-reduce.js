ba98e09984c39eae676adb12c526996e52614b25
Stephanie Cherubin <seraphin.cherubin@gmail.com>
2019-05-16 22:35:24 -0700

    update functions

--- src/map-filter-reduce.js
+++ src/map-filter-reduce.js
@@ -1,41 +1,36 @@
-import inventory, { categories } from './inventory'
+import inventory, { categories } from './Inventory';
 
 
 // ==========================================================
 // Reduce inventory to count number of items in each category
 function countCategory(items, category) {
   return items.reduce((total, item) => {
-    return item.category === category ? total += 1 : total
-  }, 0)
+    return item.category === category ? total += 1 : total;
+  }, 0);
 }
 
 categories.forEach((cat) => {
-  console.log(cat, countCategory(inventory, cat))
-})
+  console.log(cat, countCategory(inventory, cat));
+});
 
 // ==========================================================
 // Filter inventory on category
 
 function filterOnCategory(items, category) {
-  return items.filter((item) => {
-    return item.category === category
-  })
+  return items.filter(item => item.category === category);
 }
 
 categories.forEach((cat) => {
-  console.log(cat, filterOnCategory(inventory, cat))
-})
+  console.log(cat, filterOnCategory(inventory, cat));
+});
 
 // ==========================================================
 // Map inventory from object to string of name and price
 
 function mapInventoryToString(items) {
-  return items.map((item) => {
-    return `${item.name} $${item.price}`
-  })
+  return items.map(item => `${item.name} $${item.price}`);
 }
 
 mapInventoryToString(inventory).forEach((item) => {
-  console.log(item)
-})
-
+  console.log(item);
+});
