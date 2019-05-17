fb5cab872a2cf3b44d9b82740960cbaef92adb9c
Stephanie Cherubin <seraphin.cherubin@gmail.com>
2019-05-16 22:34:11 -0700

    add Product Detail and Footer components and routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-/* eslint-disable react/jsx-no-bind */
 /* eslint-disable no-shadow */
 /* eslint-disable react/no-array-index-key */
-/* eslint-disable react/jsx-filename-extension */
+
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
@@ -9,6 +8,9 @@ import './App.css';
 import Home from './Home';
 import Navbar from './Navbar';
 import Shop from './Shop';
+import ProductDetail from './ProductDetail';
+import Footer from './Footer';
+import './Footer.css';
 import './Categories.css';
 
 const App = () => (
@@ -17,6 +19,8 @@ const App = () => (
       <Navbar />
       <Route path="/" exact component={Home} />
       <Route path="/shop" exact component={Shop} />
+      <Route path="/product/:id" exact component={ProductDetail} />
+      <Footer />
     </div>
   </BrowserRouter>
 );
