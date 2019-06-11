500f9f8555abd974c352401b86d73b2f74f28b02
Stephanie Cherubin <seraphin.cherubin@gmail.com>
2019-06-11 11:26:09 -0700

    change from function to class

--- src/Product.js
+++ src/Product.js
@@ -1,37 +1,58 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
+import HeartCheckbox from 'react-heart-checkbox';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import './Product.css';
 
-const Product = ({
-  id, title, desc, price, isSelected, addToWishList,
-}) => (
-  <div className="product">
-    <Link to={`product/${id}`}>
-      <img
-        alt="product_image"
-        className="productImage"
-        src={`https://picsum.photos/200?random=${id}`}
-      />
-    </Link>
-    <h3>{title}</h3>
-    <p className="description">{desc}</p>
-    <p className="price">
-      $
-      {price}
-    </p>
-    <input
-      type="checkbox"
-      checked={isSelected}
-      onClick={(e) => {
-        // console.log(`${id} was clicked`)
-        addToWishList();
-      }}
-    />
-    <br />
-  </div>
-);
+
+class Product extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      checked: false
+    }
+  }
+
+  render() {
+  
+    const { checked } = this.state;
+    const { id, title, desc, price, isSelected, addToWishList } = this.props;
+    
+    return (
+
+      <div className="product">
+        <Link to={`product/${this.props.id}`}>
+          <img
+            alt="product_image"
+            className="productImage"
+            src={`https://picsum.photos/200?random=${id}`}
+          />
+        </Link>
+        <h3>{title}</h3>
+        <p className="description">{desc}</p>
+        <p className="price">
+          $
+          {price}
+        </p>
+        <input
+          type="checkbox"
+          checked={isSelected}
+          className="red-heart-checkbox"
+          onClick={(e) => {
+            // console.log(`${id} was clicked`)
+            addToWishList();
+          }}
+        />
+        <br />
+        <FontAwesomeIcon icon={faHeart} />
+      </div>
+    )
+  }
+}
 
 Product.propTypes = {
   id: PropTypes.number.isRequired,
